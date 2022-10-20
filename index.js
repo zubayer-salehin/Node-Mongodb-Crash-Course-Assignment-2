ebef606239e2a1abe00dea182f892cfbdfdb1c8d
zubayer-salehin <freelancerzubear@gmail.com>
2022-10-21 01:20:11 +0600

    server run problame find out

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const app = require("./app");
-const dbConnect = require('./utils/dbConnect');
-
-// database connection
-dbConnect();
-
-// server
+const DBConnect = require('./utils/dbConnect');
 const port = process.env.PORT || 8080;
 
+
+// database connection
+DBConnect();
+
+
+// server
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
