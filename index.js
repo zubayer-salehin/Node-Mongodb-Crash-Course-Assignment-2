4fc4ea3af3a5a5790190c00efe343e3ee411e315
zubayer-salehin <freelancerzubear@gmail.com>
2022-10-21 02:04:37 +0600

    server problame find

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const tourRouter = require("./Routes/Tour.Route");
+const errorHandler = require("./Middleware/errorHandler");
 require('dotenv').config();
-const app = require("./app");
 const DBConnect = require('./utils/dbConnect');
 const port = process.env.PORT || 8080;
 
 
+/* Application Middleware */
+app.use(express.json())
+app.use(cors())
+
+
 // database connection
 DBConnect();
+
+
+/* Home Route */
+app.get('/', (req, res) => {
+    res.send("Tour Management Website");
+})
+
+
+
+/* All Tour Route */
+app.use("/tours", tourRouter);
+
+
+
+/* Undefined Route */
+app.all('*', (req, res) => {
+    res.send('No Route Found')
+})
+
+
+/* Global Error Handler*/
+app.use(errorHandler);
+
+
+process.on("unhandledRejection", (error) => {
+    console.log(error.name, error.message);
+    app.close(() => {
+        process.exit(1);
+    })
+})
 
 
 // server
