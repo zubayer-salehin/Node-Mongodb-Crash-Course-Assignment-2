2e4f185c88b499c6b77b1d160abeca6f1daef4d4
zubayer-salehin <freelancerzubear@gmail.com>
2022-11-24 11:22:54 +0600

    mongodb connection url modify

--- Utils/dbConnect.js
+++ Utils/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function DBConnect() {
-    mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.5pmu7.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log(`Mongoess Connection Successfully`);
         })
